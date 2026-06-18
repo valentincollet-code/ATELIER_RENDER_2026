9ba3adb141c1f3055e226cf6c3f089cfc64530a8
valentincollet-code <v.colletpro@gmail.com>
2026-06-18 21:42:02 +0200

    Add /info and /env routes to the Flask app
    Added new routes for app info and environment variables.

--- app/app.py
+++ app/app.py
@@ -11,6 +11,17 @@ def home():
 def health():
     return {"status": "Tout est ok ou pas"}
 
+@app.route("/info")
+def info():
+    return {
+        "app": "Flask Render",
+        "student": "Valentin COLLET",
+        "version": "v1"
+    }
+
+@app.route("/env")
+def env():
+    return {"env": os.getenv("ENV")}
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
