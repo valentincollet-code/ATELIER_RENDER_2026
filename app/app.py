3bc7d843c36cb2e39a06e942d1a19bb4d0cb61e3
valentincollet-code <v.colletpro@gmail.com>
2026-06-18 23:15:40 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,53 +1,29 @@
-terraform {
-  required_providers {
-    render = {
-      source  = "render-oss/render"
-      version = ">= 1.7.0"
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Flask + Docker + GHCR + Terraform + Render"
+
+@app.route("/health")
+def health():
+    return {"status": "Tout est ok ou pas"}
+
+@app.route("/info")
+def info():
+    return {
+        "app": "Flask Render",
+        "student": "Valentin COLLET",
+        "version": "v1"
     }
-  }
-}
 
-provider "render" {
-  api_key  = var.render_api_key
-  owner_id = var.render_owner_id
-}
+@app.route("/env")
+def env():
+    return {"env": os.getenv("ENV")}
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
 
-variable "github_actor" {
-  description = "GitHub username"
-  type        = string
-}
-
-resource "render_web_service" "flask_app" {
-  name   = "flask-render-iac-${var.github_actor}"
-  plan   = "free"
-  region = "frankfurt"
-  
-  env_vars = {
-    ENV = {
-      value = "production"
-    }
-    DATABASE_URL = {
-      value = "postgresql://valentin_database_user:1bAmOAhOXttGAFOvJZMI85RlfnWHmWDL@dpg-d8q4tru7r5hc73el89fg-a.frankfurt-postgres.render.com/valentin_database"
-    }
-  }
-
-  runtime_source = {
-    image = {
-      image_url = var.image_url
-      tag       = var.image_tag
-    }
-  }
-}
-
-resource "render_web_service" "adminer" {
-  name        = "adminer-${var.github_actor}"
-  plan        = "free"
-  region      = "frankfurt"
-
-  runtime_source = {
-    image = {
-      image_url = "docker.io/library/adminer"
-      tag       = "latest"
-    }
-  }
-}
+    app.run(host="0.0.0.0", port=port)
