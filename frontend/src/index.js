ade173f694af94f6c08794f5031bb8eeb36458b4
valentincollet-code <v.colletpro@gmail.com>
2026-06-18 23:51:16 +0200

    Initialize React application in index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Frontend Valentin - Étape 4 Réussie !</h1>
+      <p>L'application React est connectée et déployée sur Render.</p>
+    </div>
+  </React.StrictMode>
+);
